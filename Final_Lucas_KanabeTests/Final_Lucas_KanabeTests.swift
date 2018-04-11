38ad167c5627ada14440830717c2debcc28f2e59
Kanabe Lucas A <n01093689@l116-n-039.mediastudies.ca>
2018-04-11 15:54:29 -0400

    Initial Commit

---
+++ Final_Lucas_KanabeTests/Final_Lucas_KanabeTests.swift
@@ -0,0 +1,36 @@
+//
+//  Final_Lucas_KanabeTests.swift
+//  Final_Lucas_KanabeTests
+//
+//  Created by Kanabe Lucas A. on 4/11/18.
+//  Copyright © 2018 Kanabe Lucas A. All rights reserved.
+//
+
+import XCTest
+@testable import Final_Lucas_Kanabe
+
+class Final_Lucas_KanabeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
